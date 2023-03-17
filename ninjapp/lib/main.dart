b7bf0dff4247410d40704c619ea384b21cda7d7d
Lukam90 <lucien.hamm@gmail.com>
2023-03-17 12:25:07 +0100

    MAJ Ex Stateful Widgets

--- ninjapp/lib/main.dart
+++ ninjapp/lib/main.dart
@@ -8,7 +8,16 @@ void main() {
   );
 }
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
+  const NinjaCard({super.key});
+
+  @override
+  _NinjaCardState createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  int ninjaLevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +27,15 @@ class NinjaCard extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            ninjaLevel += 1;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
@@ -61,7 +79,7 @@ class NinjaCard extends StatelessWidget {
             ),
             SizedBox(height: 10),
             Text(
-              "8",
+              "$ninjaLevel",
               style: TextStyle(
                 color: Colors.amberAccent[200],
                 letterSpacing: 2,
